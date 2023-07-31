f424c29006962e93b0cb832974849d4c49d1f8d2
Amin Siddique <amin.siddique95@gmail.com>
2023-07-31 16:47:55 +0530

    update

--- code/dags/example.py
+++ code/dags/example.py
@@ -1,25 +1,11 @@
 from __future__ import annotations
-
-# [START tutorial]
-# [START import_module]
 from datetime import datetime, timedelta
 from textwrap import dedent
-
-# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-
-# Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 
-# [END import_module]
-
-
-# [START instantiate_dag]
 with DAG(
     "tutorial",
-    # [START default_args]
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
     default_args={
         "depends_on_past": False,
         "email": ["airflow@example.com"],
@@ -51,53 +37,11 @@ with DAG(
 
     # t1, t2 and t3 are examples of tasks created by instantiating operators
     # [START basic_task]
-[docs]    t1 = BashOperator(
+    t1 = BashOperator(
         task_id="print_date",
         bash_command="date",
     )
 
-    t2 = BashOperator(
-        task_id="sleep",
-        depends_on_past=False,
-        bash_command="sleep 5",
-        retries=3,
-    )
-    # [END basic_task]
+   
 
-    # [START documentation]
-    t1.doc_md = dedent(
-        """\
-    #### Task Documentation
-    You can document your task using the attributes `doc_md` (markdown),
-    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-    rendered in the UI's Task Instance Details page.
-    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
-    **Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
-    """
-    )
-
-    dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG; OR
-    dag.doc_md = """
-    This is a documentation placed anywhere
-    """  # otherwise, type it like this
-    # [END documentation]
-
-    # [START jinja_template]
-    templated_command = dedent(
-        """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-    {% endfor %}
-    """
-    )
-
-    t3 = BashOperator(
-        task_id="templated",
-        depends_on_past=False,
-        bash_command=templated_command,
-    )
-    # [END jinja_template]
-
-    t1 >> [t2, t3]
-# [END tutorial]
+t1 
