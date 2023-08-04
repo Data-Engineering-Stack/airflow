af027dbef09c5440ab359a12aeed4e9460864854
Amin Siddique <amin.siddique95@gmail.com>
2023-08-05 01:46:49 +0530

    Update dynamic.py

--- code/dags/dynamic.py
+++ code/dags/dynamic.py
@@ -7,11 +7,11 @@ from airflow.operators.python import  PythonOperator
 import random
 
 d={}
-op_args=[]
+kwargs=[]
 for i in range(5):
     d['filename']=i
     d['tablename']=i+10
-    op_args.append(d)
+    kwargs.append(d)
 
 
 def dum(filename,tablename):
@@ -25,6 +25,6 @@ with DAG(dag_id="example_dynamic_task_mapping", start_date=datetime(2022, 3, 4),
     python_test = PythonOperator.partial(
             task_id="python_test_task",
             python_callable=dum,
-        ).expand(op_kwargs=op_args)
+        ).expand(op_kwargs=kwargs)
 ##.expand(op_kwargs=XComArg(some_previous_task, key='return_value'))
 python_test
