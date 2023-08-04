48e169c7c6bf92dc0abde3ed7257b6db7aa19973
Amin Siddique <amin.siddique95@gmail.com>
2023-08-05 01:44:45 +0530

    Update dynamic.py

--- code/dags/dynamic.py
+++ code/dags/dynamic.py
@@ -4,29 +4,27 @@ from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.python import  PythonOperator
+import random
 
-def dum(x):
-    print(f"hi, the number is {x}")
+d={}
+op_args=[]
+for i in range(5):
+    d['filename']=i
+    d['tablename']=i+10
+    op_args.append(d)
+
+
+def dum(filename,tablename):
+    print(f"hi, the filename is {filename} and tablename is {tablename}")
 
 
 with DAG(dag_id="example_dynamic_task_mapping", start_date=datetime(2022, 3, 4),catchup=False) as dag:
 
-    # @task
-    # def add_one(x: int):
-    #     return x + 1
-
-    # @task
-    # def sum_it(values):
-    #     total = sum(values)
-    #     print(f"Total was {total}")
-
-    # added_values = add_one.expand(x=[1, 2, 3])
-    # sum_it(added_values)
 
     
     python_test = PythonOperator.partial(
             task_id="python_test_task",
             python_callable=dum,
-        ).expand(op_kwargs=[{'x':'1'}, {'x':'2'}, {'x':'3'}])
+        ).expand(op_kwargs=op_args)
 ##.expand(op_kwargs=XComArg(some_previous_task, key='return_value'))
 python_test
