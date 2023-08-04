fd405eafcf47a412e26c3f99572b381db2ad83be
Amin Siddique <amin.siddique95@gmail.com>
2023-08-05 01:15:28 +0530

    Update dynamic.py

--- code/dags/dynamic.py
+++ code/dags/dynamic.py
@@ -22,11 +22,9 @@ with DAG(dag_id="example_dynamic_task_mapping", start_date=datetime(2022, 3, 4),
     # sum_it(added_values)
 
     
-    @task 
-    def x():
-        task1 = PythonOperator.partial(
+    python_test = PythonOperator.partial(
             task_id="python_test_task",
-            python_callable=lambda: print(f'Hi {x}, from python operator'),
+            python_callable=lambda: print('H from python operator'),
         ).expand_kwargs([{'x':1}, {'x':2}, {'x':3}])
-        
-    x()
+
+python_test
