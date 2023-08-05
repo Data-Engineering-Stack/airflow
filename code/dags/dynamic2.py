b853f6ac42efb6b47b3f5e772123360fe6630cc5
Amin Siddique <amin.siddique95@gmail.com>
2023-08-05 21:32:59 +0530

    Create dynamic2.py

---
+++ code/dags/dynamic2.py
@@ -0,0 +1,59 @@
+"""Example DAG demonstrating the usage of dynamic task mapping."""
+from __future__ import annotations
+from datetime import datetime
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.python import  PythonOperator
+from airflow.decorators import task
+from airflow.operators.python import get_current_context
+from airflow.models.param import Param
+
+tableschema= 'ABCD'
+filename = "{{ params.filename }}"
+filename = "{{ params.target_table }}"
+
+
+init = {
+    'env': 'dev',
+    'notification_emails' : 'airflow@airflow.net',
+    's3_conn_id' : 's3_conn'
+
+}
+
+def ops(init,file_name,tablename,tableschema,**kwargs):
+    print(f"{init}, the filename is {filename} and tablename is {tableschema}.{tablename}")
+
+
+with DAG(dag_id="test_expand", 
+         start_date=datetime(2022, 3, 4),
+         schedule_interval=None,
+         params={
+             'filename': Param("",type='string'),
+             'target_table': Param("",type='string')
+         },
+         catchup=False
+) as dag:
+
+    @task
+    def generate_args(init,tableschema):
+        context = get_current_context()
+        print(context)
+        config = context['dag_run'].conf
+        list_kwargs = []
+
+        op_kwargs={}
+        op_kwargs['init'] = init
+        op_kwargs['file_name'] = config['filename']
+        op_kwargs['tablename'] = config['tablename']
+        op_kwargs['tableschema'] = tableschema
+        list_kwargs.append(op_kwargs)
+        return op_kwargs
+
+    op_kwargs = generate_args(init,tableschema)
+
+    python_test = PythonOperator.partial(
+            task_id="python_test_task",
+            python_callable=ops,
+        ).expand(op_kwargs=op_kwargs)
+
+python_test
