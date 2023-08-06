11dbb2ddecc1972b7a6664b58bc6ec76fac11179
Amin Siddique <amin.siddique95@gmail.com>
2023-08-06 18:34:14 +0530

    update

---
+++ code/dags/spark.py
@@ -0,0 +1,18 @@
+import datetime
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+app = 'spark.py'
+
+
+with DAG('spark_job',schedule=None,start_date=datetime(2022,01,01),catchup=False) as dag:
+
+
+    SparkSubmitOperator_task = SparkSubmitOperator(
+    task_id='SparkSubmitOperator_task',
+    application=app,
+    conf=None,
+    conn_id='spark_Conn'
+    )
+
+SparkSubmitOperator_task
