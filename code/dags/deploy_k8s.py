dca1f6f274aee41cb92dcfed1344a9453ce1587e
Amin Siddique <amin.siddique95@gmail.com>
2023-08-06 23:51:22 +0530

    Update deploy_k8s.py

--- code/dags/deploy_k8s.py
+++ code/dags/deploy_k8s.py
@@ -20,11 +20,11 @@ def get_executor_config():
                 ports=[k8s.V1ContainerPort(container_port=4040),k8s.V1ContainerPort(container_port=7337)]
                 + [k8s.V1ContainerPort(container_port=port) for port in range(42000,42049)],
                 env=[
-                    k8s.V1EnvVar(name="PYSPARK_PYTHON",
-                                 value="/opt/bitname/python/bin/python"
+                    k8s.V1EnvVar(name="JAVA_HOME",
+                                 value="usr/lib/jvm/java-11-openjdk-amd64/"
                                  ),
                     k8s.V1EnvVar(name="SPARK_HOME",
-                                 value="/home/airflow/.local/lib/python3.8/site-packages/pyspark"
+                                 value="/home/airflow/.local/lib/python3.10/site-packages/pyspark"
                                  ),
                     k8s.V1EnvVar(name="PYSPARK_DRIVER_PYTHON",value=sys.executable),
                 ],
