a21668260e8eb183029240f7bfd1e1210ee65e1d
Amin Siddique <amin.siddique95@gmail.com>
2023-08-06 19:38:59 +0530

    Update spark.py

--- code/dags/spark.py
+++ code/dags/spark.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow.models import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-app = '/opt/airflow/dags/repo/code/dags/spark.py'
+app = '/opt/airflow/dags/repo/code/dags/sparkcode.py'
 
 
 with DAG('spark_job',schedule=None,start_date=datetime(2022, 3, 4),catchup=False) as dag:
