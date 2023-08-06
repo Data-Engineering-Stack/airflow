af69ab2c98dbde752b42132477e35e35d98e65fd
Amin Siddique <amin.siddique95@gmail.com>
2023-08-06 18:37:12 +0530

    Update spark.py

--- code/dags/spark.py
+++ code/dags/spark.py
@@ -12,7 +12,7 @@ with DAG('spark_job',schedule=None,start_date=datetime(2022, 3, 4),catchup=False
     task_id='SparkSubmitOperator_task',
     application=app,
     conf=None,
-    conn_id='spark_Conn'
+    conn_id='spark_conn'
     )
 
 SparkSubmitOperator_task
