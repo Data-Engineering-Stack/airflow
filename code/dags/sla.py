54c26f87083c799bfd3807318baff4d7391883f8
Amin Siddique <amin.siddique95@gmail.com>
2023-08-19 17:08:27 +0530

    update

--- code/dags/sla.py
+++ code/dags/sla.py
@@ -1,48 +1,46 @@
-"""
-A simple example showing the basics of using a custom SLA notification response.
-"""
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from datetime import timedelta, datetime
+import time
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+
+"""Example DAG demonstrating SLA use in Tasks"""
 
 
-# Our notification function which simply prints out some fo the information passed in to the SLA notification miss.
-def print_sla_miss(dag, task_list, blocking_task_list, slas, blocking_tis):
-    print(f'SLA was missed on DAG {dag}')
-
-# Set up some simple DAG args, note the email is set to None to avoid the missed SLA from creating a failure in the
-# DAG. SLA emails are sent out if the SLA is missed and the email is defined, there is currently not a flag to disable.
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2017, 1, 1),
-    'email': None,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0
-}
-
-# Create a basic DAG with our args
-with DAG(
-    'basic_sla',
-    default_args=default_args,
-    # Add our method as a SLA callback
-    sla_miss_callback=print_sla_miss,
-    # A common interval to make the job fire when we run it
-    schedule_interval='30 11 * * *',
-    catchup=False
-) as dag:
-
-# Add a task that will always fail the SLA
-    t1 = BashOperator(
-        task_id='timeout',
-        # Set our task up with a 10 second SLA
-        sla=timedelta(seconds=10),
-        # Sleep 15 seconds to guarantee we miss the SLA
-        bash_command='sleep 15',
-        # Do not retry so the SLA miss fires after the first execution
-        retries=0,
-        dag=dag,
+# [START howto_task_sla]
+def sla_callback(dag, task_list, blocking_task_list, slas, blocking_tis):
+    print(
+        "The callback arguments are: ",
+        {
+            "dag": dag,
+            "task_list": task_list,
+            "blocking_task_list": blocking_task_list,
+            "slas": slas,
+            "blocking_tis": blocking_tis,
+        },
     )
 
-    t1
+
+@dag(
+    schedule_interval="*/2 * * * *",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    sla_miss_callback=sla_callback,
+    default_args={'email': "email@example.com"},
+)
+def example_sla_dag():
+    @task(sla=timedelta(seconds=10))
+    def sleep_20():
+        """Sleep for 20 seconds"""
+        time.sleep(20)
+
+    @task
+    def sleep_30():
+        """Sleep for 30 seconds"""
+        time.sleep(30)
+
+    sleep_20() >> sleep_30()
+
+
+dag = example_sla_dag()
+
+# [END howto_task_sla]
