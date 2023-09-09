3aaaeafb79e8255a4a2e4b38c516e7e13546d6a0
Amin Siddique <amin.siddique95@gmail.com>
2023-09-09 16:14:37 +0530

    Update pg.py

--- code/dags/pg.py
+++ code/dags/pg.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from datetime import datetime
+from datetime import datetime,timedelta
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
 from airflow.operators.email_operator import EmailOperator
@@ -8,10 +8,34 @@ from airflow.operators.python import get_current_context
 
 postgres_conn_id='internal_postgres'
 
+init = {
+    'notification_emails' : 'aminsiddique95@gmail.com'
+}
+
+default_args={
+    "depends_on_past": False,
+    # "email": ["airflow@example.com"],
+    # "email_on_failure": True,
+    # "email_on_retry": False,
+    # "retries": 1,
+    # "retry_delay": timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+     'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    #'sla': timedelta(seconds=5),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function, # or list of functions
+    # 'on_success_callback': some_other_function, # or list of functions
+    # 'on_retry_callback': another_function, # or list of functions
+    # 'sla_miss_callback': yet_another_function, # or list of functions
+    # 'trigger_rule': 'all_success'
+}
 
 # Define the SQL query you want to execute
 sql_query = """
- select * from dag_run;
+ select * from dag_run limit 1;
 """
 
 def my_task():
@@ -22,14 +46,14 @@ def my_task():
     html_content = f""" Please find the below dags:\n\n{df_html}"""
     email_task = EmailOperator(
     task_id='send_email_task',
-    to='aminsiddique95@gmail.com',  # Replace with the recipient's email address
+    to=init['notification_emails'],  # Replace with the recipient's email address
     subject='DataFrame Table in Email',
     html_content=html_content,  # HTML content with the DataFrame table
     )
 
     email_task.execute(get_current_context())
 
-    return df_html
+    return True
 
 
 
@@ -37,19 +61,13 @@ def my_task():
 
 with DAG(
     'query_postgres_table',
-    start_date=datetime(2023, 9, 9),  # Adjust the start date accordingly
-    schedule_interval=None,  # Set your desired schedule interval or None for manual execution
+    start_date=datetime(2023, 9, 9),  
+    schedule_interval=None, 
+    default_args=default_args,
     catchup=False  # Set to False if you don't want to backfill
 ) as dag:
 
 
-# Create a PostgresOperator to execute the query
-    # query_task = PostgresOperator(
-    #     task_id='query_postgres_task',
-    #     postgres_conn_id=postgres_conn_id,  # Specify your Postgres connection ID
-    #     sql=sql_query,
-    #     dag=dag
-    # )
     run_this = PythonOperator(
         task_id='postgres_task',
         python_callable=my_task,
