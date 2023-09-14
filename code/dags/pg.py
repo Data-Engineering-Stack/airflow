7de41b0602076a4312b701efd069055485a2b88a
Amin Siddique <amin.siddique95@gmail.com>
2023-09-15 04:55:07 +0530

    Update pg.py

--- code/dags/pg.py
+++ code/dags/pg.py
@@ -96,7 +96,7 @@ with DAG(
     user_defined_macros={"my_macro":get_prev_state}
 ) as dag:
 
-    state = "'{{ get_prev_state(postgres_task) }}'"
+    state = "'{{ my_macro(postgres_task) }}'"
 
     bash_task = BashOperator(
         task_id="bash_task",
