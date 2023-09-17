607922c89a4127d46f6896566fef8b4a0e684a54
Amin Siddique <amin.siddique95@gmail.com>
2023-09-17 17:08:29 +0530

    Update dag_inspector.py

--- code/dags/dag_inspector.py
+++ code/dags/dag_inspector.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import get_current_context
+from airflow.models.param import Param
+
 
 postgres_conn_id='internal_postgres'
 
@@ -31,22 +33,22 @@ default_args={
     # 'trigger_rule': 'all_success'
 }
 
+dag_list_id = "{{ params.dag_list }}"
+
 
 
 
 def get_all_dags(dag):
     ''' returns list of all dags'''
     dag_list = []
-    sql = f""" select dag_id from dag where dag_id != {dag.dag_id} and is_active=True; """
+    sql = f""" select dag_id from dag where dag_id != '{dag.dag_id}' and is_active=True; """
     db_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
-
     res = db_hook.get_records(sql)
 
     for row in res:
         dag_id = row[0]
         print(dag_id)
         dag_list.append(dag_id)
-    
 
     return dag_list
 
@@ -56,10 +58,24 @@ with DAG(
     start_date=datetime(2023, 9, 9),  
     schedule_interval=None, 
     default_args=default_args,
+    doc_md="Dag to trigger n number of dags simultanelously",
     catchup=False,  
     max_active_runs=1,
+    params={
+        "dag_id_list" : Param("",type="list",description="provide list of dag_ids in list to trigger")
+    }
 ) as dag:
 
+    def verify_input(**kwargs):
+        config = kwargs["dag_run"].conf
+
+        print(config)
+        if not bool(config):
+            return get_all_dags(dag)
+        else:
+            return config
+
+    get_all_dags = verify_input()
 
     @task(task_id="dag_triggerer")
     def dag_triggerer(dag_id):
@@ -70,6 +86,6 @@ with DAG(
                 )
         trigger.execute(context=get_current_context())
 
-    dag_triggerer = dag_triggerer.expand(dag_id=get_all_dags(dag))
+    dag_triggerer = dag_triggerer.expand(dag_id=get_all_dags)
 
 dag_triggerer
