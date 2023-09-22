618ed71c45919fc9558d33ee621aec92836a2c68
Amin Siddique <amin.siddique95@gmail.com>
2023-09-22 22:56:32 +0530

    Update timedelta.py

--- code/dags/timedelta.py
+++ code/dags/timedelta.py
@@ -10,7 +10,12 @@ import pendulum
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.sensors.time_delta import TimeDeltaSensor
-from datetime import timedelta
+from datetime import timedelta,datetime
+from airflow.operators.python import PythonOperator
+
+def printer():
+    print(datetime.now())
+    return True
 
 with DAG(
     dag_id="TimeDeltaSensorExample",
@@ -19,12 +24,20 @@ with DAG(
     catchup=False,
     tags=["example"],
 ) as dag:
-    wait = TimeDeltaSensor(task_id="wait", 
-                           delta=datetime.timedelta(seconds=30),
-                           mode='poke',
-                           poke_interval=60,
-                           timeout=timedelta(minutes=5)
-                           
-                           )
-    finish = EmptyOperator(task_id="finish")
-    wait >> finish
+    wait = TimeDeltaSensor(
+        task_id="wait", 
+        delta=datetime.timedelta(seconds=30),
+        mode='poke',
+        poke_interval=60,
+        timeout=timedelta(minutes=5)
+        )
+
+    execute_task = PythonOperator(
+        task_id='execute_task',
+        python_callable=printer,
+        provide_context=True,
+        dag=dag
+    )
+
+
+    wait >> execute_task
