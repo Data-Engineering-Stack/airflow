0f429049e0a83d4dc78547b057f4e0754a65565f
Amin Siddique <amin.siddique95@gmail.com>
2023-10-29 01:42:30 +0530

    Update spark.py

--- code/dags/spark.py
+++ code/dags/spark.py
@@ -6,15 +6,26 @@ from deploy_k8s import get_executor_config
 app = '/opt/airflow/dags/repo/code/dags/sparkcode.py'
 
 
+spark_configurations = {
+    "spark.driver.port": "42000",  # The name of your Spark application.
+    "spark.driver.blockManager.port": "42016",  # The name of your Spark application.
+    "spark.blockManager.port": "42032",  # The name of your Spark application.
+    "spark.executor.memory": "4g",  # Memory allocated per executor.
+    "spark.driver.memory": "4g",  # Memory allocated for the driver.
+    "spark.serializer": "org.apache.spark.serializer.KryoSerializer",  # Serializer for data.
+}
+
+
 with DAG('spark_job',schedule=None,start_date=datetime(2022, 3, 4),catchup=False) as dag:
 
 
     SparkSubmitOperator_task = SparkSubmitOperator(
     task_id='SparkSubmitOperator_task',
     application=app,
-    conf=None,
+    executor_memory="4g",
+    conf=spark_configurations,
     conn_id='spark_conn',
-    verbose=False,
+    verbose=True,
     name='testspark',
     executor_config=get_executor_config()
     )
