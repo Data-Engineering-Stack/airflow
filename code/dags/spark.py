1dcfbf91b65fd8ddeb79ecd43e2f4c68e59faf55
Amin Siddique <amin.siddique95@gmail.com>
2023-10-29 15:46:59 +0530

    Update spark.py

--- code/dags/spark.py
+++ code/dags/spark.py
@@ -7,14 +7,14 @@ app = '/opt/airflow/dags/repo/code/dags/sparkcode.py'
 
 
 spark_configurations = {
-    "spark.driver.port": "42000",  # The name of your Spark application.
+    # "spark.driver.port": "42000",  # The name of your Spark application.
     "spark.driver.blockManager.port": "42016",  # The name of your Spark application.
     "spark.blockManager.port": "42032",  # The name of your Spark application.
     "spark.executor.memory": "4g",  # Memory allocated per executor.
     "spark.driver.memory": "4g",  # Memory allocated for the driver.
     "spark.serializer": "org.apache.spark.serializer.KryoSerializer",  # Serializer for data.
     "spark.dynamicAllocation.enabled":"false",
-    "spark.driver.host" : "10.110.79.128"
+    "spark.driver.host" : "10.1.5.34"
 }
 
 
